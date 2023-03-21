3aa281e8a3c4862e135e75948a75bf121a1268a1
BangSeokHyun <stuban175@gmail.com>
2023-03-21 15:30:49 +0900

    FSM 구조 추가

---
+++ Assets/Scripts/Util/FSM/FSMState.cs
@@ -0,0 +1,32 @@
+using System;
+
+
+namespace QT
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FSMStateAttribute : System.Attribute
+    {
+        public readonly int Key;
+
+        public FSMStateAttribute(int key)
+        {
+            this.Key = key;
+        }
+    }
+
+
+    public abstract class FSMState<T> where T : IFSMEntity
+    {
+        protected readonly T _ownerEntity;
+
+        public virtual void InitializeState() { }
+        public virtual void UpdateState() { }
+        public virtual void FixedUpdateState() { }
+        public virtual void ClearState() { }
+
+        public FSMState(IFSMEntity owner)
+        {
+            _ownerEntity = (T)owner;
+        }
+    }
+}
