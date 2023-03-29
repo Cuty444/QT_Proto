5c88132f51201bfa875cfda3f426b5b55efa8e42
BangSeokHyun <stuban175@gmail.com>
2023-03-29 23:28:59 +0900

    투사체 프리팹 추가 및 연결

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -18,15 +18,15 @@ namespace QT
         private int _bounceCount;
         
         private Vector2 _direction;
+        
 
-
-        public void Init(float speed, float speedDecay, float size, int maxBounce,Vector2 direction)
+        public void Init(ProjectileGameData data, Vector2 dir, int maxBounce)
         {
-            _speed = speed;
-            _speedDecay = speedDecay;
-            _direction = direction;
-            _size = size;
-
+            _speed = data.InitalSpd;
+            _speedDecay = data.SpdDecay;
+            _size = data.ColliderRad;
+            
+            _direction = dir;
             _bounceCount = _maxBounce = maxBounce;
         }
 
@@ -42,7 +42,7 @@ namespace QT
             var moveLength = _speed * Time.deltaTime;
             var hit = Physics2D.CircleCast(transform.position, _size, _direction, moveLength, _bounceMask);
 
-            if (hit != null)
+            if (hit.collider != null)
             {
                 _direction += hit.normal * (-2 * Vector2.Dot(_direction, hit.normal));
 
--- Assets/Scripts/Projectile/ProjectileShooter.cs
+++ Assets/Scripts/Projectile/ProjectileShooter.cs
@@ -13,9 +13,11 @@ public enum AimTypes
 
 public class ProjectileShooter : MonoBehaviour
 {
+    [SerializeField] private GameObject _projectilePrefab; // Todo: 오브젝트 풀링 기능이 포함된 리소스 매니저 구현 필요
+    
     [SerializeField] protected Transform _shootPoint;
 
-    private Transform _targetTransform;
+    [SerializeField] private Transform _targetTransform;
 
     public void SetTarget(Transform target)
     {
@@ -33,7 +35,17 @@ public class ProjectileShooter : MonoBehaviour
 
         foreach (var shoot in shootData)
         {
+            var projectileData = SystemManager.Instance.DataManager.GetDataBase<ProjectileGameDataBase>().GetData(shoot.ProjectileDataId);
+            if (projectileData == null)
+            {
+                continue;
+            }
+            
+            // Todo: 오브젝트 풀링 기능이 포함된 리소스 매니저 구현 필요
+            var projectile = Instantiate(_projectilePrefab, _shootPoint.position, Quaternion.identity).GetComponent<Projectile>();
+            
             var dir = GetDirection(shoot.ShootAngle, aimType);
+            projectile.Init(projectileData, dir, shoot.MaxBounceCount);
         }
     }
 
