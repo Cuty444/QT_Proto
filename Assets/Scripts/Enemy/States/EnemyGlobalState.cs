e287021a8ba38cbf5d6c3ac48297e6e23e9c7c1e
BangSeokHyun <stuban175@gmail.com>
2023-04-04 04:15:10 +0900

    투사체 상태와 경직 상태 역할 변경

--- Assets/Scripts/Enemy/States/EnemyGlobalState.cs
+++ Assets/Scripts/Enemy/States/EnemyGlobalState.cs
@@ -12,27 +12,15 @@ namespace QT.Enemy
             _ownerEntity.OnDamageEvent.AddListener(OnDamage);
         }
         
-        private  bool _isRiged = false;
-
         private void OnDamage(float damage, Vector2 hitPoint)
         {
-            if (_ownerEntity.CurrentState == (int)Enemy.States.Dead)
+            if (_ownerEntity.CurrentState >= (int)Enemy.States.Rigid)
             {
                 return;
             }
 
-            if (_ownerEntity.CurrentState != (int) Enemy.States.Rigid)
-            {
-                _ownerEntity.HP.AddStatus(-damage);
-                if (_ownerEntity.HP > 0)
-                {
-                    _ownerEntity.ChangeState(Enemy.States.Rigid);
-                }
-                else
-                {
-                    _ownerEntity.ChangeState(Enemy.States.Projectile);
-                }
-            }
+            _ownerEntity.HP.AddStatus(-damage);
+            _ownerEntity.ChangeState(Enemy.States.Rigid);
         }
     }
 }
--- Assets/Scripts/Enemy/States/EnemyProjectileState.cs
+++ Assets/Scripts/Enemy/States/EnemyProjectileState.cs
@@ -9,37 +9,16 @@ namespace QT.Enemy
     [FSMState((int)Enemy.States.Projectile)]
     public class EnemyProjectileState : FSMState<Enemy>
     {
-        private float _deadTime = 0f;
-
-        private float _deadAfterStunTime;
-        
         public EnemyProjectileState(IFSMEntity owner) : base(owner)
         {
         }
 
         public override void InitializeState()
         {
-            _deadTime = Time.time;
-            _deadAfterStunTime = SystemManager.Instance.GetSystem<GlobalDataSystem>().GlobalData.DeadAfterStunTime;
-            _ownerEntity.OnDamageEvent.AddListener(OnDamage);
         }
 
         public override void UpdateState()
         {
-            if (_deadTime + _deadAfterStunTime < Time.time)
-            {
-                _ownerEntity.ChangeState(Enemy.States.Dead);
-            }
-        }
-
-        public override void ClearState()
-        {
-            _ownerEntity.OnDamageEvent.RemoveListener(OnDamage);
-        }
-        
-        private void OnDamage(float damage, Vector2 hitPoint)
-        {
-            _ownerEntity.ChangeState(Enemy.States.Projectile);
         }
     }
 }
