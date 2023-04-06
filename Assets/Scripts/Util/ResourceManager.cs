d3b41f51c930dc4505b7d7712694906af21ab027
BangSeokHyun <stuban175@gmail.com>
2023-04-07 00:26:05 +0900

    오브젝트 풀링 기능 추가

--- Assets/Scripts/Util/ResourceManager.cs
+++ Assets/Scripts/Util/ResourceManager.cs
@@ -2,12 +2,24 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System;
+
+using Object = UnityEngine.Object;
 
 namespace QT
 {
     public class ResourceManager
     {
-        private Dictionary<string, Object> _cache = new ();
+        private Transform _poolRootTransform;
+        
+        private readonly Dictionary<string, Object> _cache = new ();
+        private readonly Dictionary<Type, Stack<MonoBehaviour>> _pool = new ();
+        
+        public void Initialize()
+        {
+            _poolRootTransform = new GameObject("PoolRoot").transform;
+            GameObject.DontDestroyOnLoad(_poolRootTransform);
+        }
 
         public async UniTask<T> LoadAsset<T>(string path, bool isCaching) where T : Object
         {
@@ -19,14 +31,55 @@ namespace QT
             }
             else
             {
-                var handle = Addressables.LoadAssetAsync<T>(path);
-            
-                await handle.Task;
-            
-                asset = handle.Result;
+                Debug.Log(path);
+                asset = await Addressables.LoadAssetAsync<T>(path);
             }
 
             return Object.Instantiate(asset);
+        }
+
+        public async UniTask<T> GetFromPool<T>(string path, Transform parent = null) where T : MonoBehaviour
+        {
+            T obj = null;
+
+            if (_pool.TryGetValue(typeof(T), out var stack))
+            {
+                if (stack.Count > 0)
+                {
+                    obj = (T) stack.Pop();
+                }
+                else
+                {
+                    obj = (await LoadAsset<GameObject>(path, true)).GetComponent<T>();
+                }
+            }
+            else
+            {
+                _pool.Add(typeof(T), new Stack<MonoBehaviour>());
+                obj = (await LoadAsset<GameObject>(path, true)).GetComponent<T>();
+            }
+
+            if (obj != null)
+            {
+                obj.transform.SetParent(parent);
+                obj.gameObject.SetActive(true);
+            }
+
+            return obj;
+        }
+
+        public void ReleaseObject<T>(T obj) where T : MonoBehaviour
+        {
+            if (_pool.TryGetValue(typeof(T), out var pool))
+            {
+                obj.gameObject.SetActive(false);
+                obj.transform.SetParent(_poolRootTransform);
+                pool.Push(obj);
+            }
+            else
+            {
+                Object.Destroy(obj.gameObject);
+            }
         }
     }
 }
