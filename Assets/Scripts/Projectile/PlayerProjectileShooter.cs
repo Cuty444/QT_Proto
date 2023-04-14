02cae5719cd3138302c4afa522bb3a4ae2e09b16
Super-Ba <superbang175@gmail.com>
2023-04-15 01:13:50 +0900

    매치되지 않는 데이터들 수정

--- Assets/Scripts/Projectile/PlayerProjectileShooter.cs
+++ Assets/Scripts/Projectile/PlayerProjectileShooter.cs
@@ -9,20 +9,19 @@ namespace QT
     public class PlayerProjectileShooter : ProjectileShooter
     {
         private const string PlayerProjectilePrefabPath = "Prefabs/PlayerProjectile.prefab";
-        public async virtual void PlayerShoot(int shootDataId, AimTypes aimType, Vector2 direction, int bounce,List<Projectile> projectiles)
+        public virtual async void PlayerShoot(int projectileDataId, AimTypes aimType, Vector2 direction, int bounce, List<Projectile> projectiles)
         {
-            var projectileData = SystemManager.Instance.DataManager.GetDataBase<ProjectileGameDataBase>()
-                .GetData(shootDataId);
+            var projectileData = SystemManager.Instance.DataManager.GetDataBase<ProjectileGameDataBase>().GetData(projectileDataId);
             if (projectileData == null)
             {
                 return;
             }
 
-            var projectile = await SystemManager.Instance.ResourceManager.GetFromPool<Projectile>(PlayerProjectilePrefabPath);
+            var projectile = await SystemManager.Instance.ResourceManager.GetFromPool<Projectile>(projectileData.PrefabPath);
 
             projectile.transform.position = _shootPoint.position;
 
-            projectile.Init(50f,0f,0.5f, direction, bounce,10,true);
+            projectile.Init(projectileData, direction, 50f, bounce);
             if (projectiles.Contains(projectile))
                 return;
             projectiles.Add(projectile);
