3fbd36d3a9b437a6fc52f5c9f1bade5420b5417d
Super-Ba <superbang175@gmail.com>
2023-04-18 00:38:51 +0900

    애니메이션 적용

--- Assets/Scripts/Projectile/EnemyProjectileShooter.cs
+++ Assets/Scripts/Projectile/EnemyProjectileShooter.cs
@@ -1,14 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using QT.Core;
+using QT.Enemy;
 using UnityEngine;
+
+using Enemy = QT.Enemy.Enemy;
 
 namespace QT
 {
     public class EnemyProjectileShooter : ProjectileShooter
     {
+        private readonly int AttackAnimHash = Animator.StringToHash("Attack");
         public override LayerMask BounceMask => LayerMask.GetMask("Wall", "Player");
+
+        private Enemy.Enemy _enemy;
         
+        public void Initialize(Enemy.Enemy enemy)
+        {
+            _enemy = enemy;
+        }
+
         public void PlayEnemyAtkSequence(int atkDataId, bool canOverlap = false)
         {
             var atkList = SystemManager.Instance.DataManager.GetDataBase<EnemyAtkGameDataBase>().GetData(atkDataId);  
@@ -31,6 +43,7 @@ namespace QT
             {
                 yield return new WaitForSeconds(data.BeforeDelay);
 
+                _enemy.Animator.SetTrigger(AttackAnimHash);
                 Shoot(data.ShootDataId, data.AimType);
                 
                 yield return new WaitForSeconds(data.AfterDelay);
