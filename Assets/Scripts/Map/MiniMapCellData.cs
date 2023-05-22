c77b3acc6348e0708ae06ab46506df2fd6d75f29
KimBeomsu <gohan2255@gmail.com>
2023-05-23 05:51:26 +0900

    Hp Image, Minimap Image 적용

--- Assets/Scripts/Map/MiniMapCellData.cs
+++ Assets/Scripts/Map/MiniMapCellData.cs
@@ -23,9 +23,11 @@ namespace QT.Map
     
     public class MiniMapCellData : MonoBehaviour
     {
+        [SerializeField] private Sprite[] _mapSprites;
         [SerializeField] private Color[] _mapColors;
         [SerializeField] private GameObject _lineRenders;
         [SerializeField] private UILineRenderer[] _uiLineRenderers;
+        [SerializeField] private Image[] _mapLineImages;
         [SerializeField] private Sprite[] _mapIconSprite;
         [SerializeField] private Transform _iconsTransform;
         [HideInInspector]public Vector2Int CellPos;
@@ -50,7 +52,6 @@ namespace QT.Map
         {
             _lineRenders.SetActive(false);
             _mapImage = GetComponent<Image>();
-            _mapImage.color = _mapColors[2];
             _dungeonMapSystem = SystemManager.Instance.GetSystem<DungeonMapSystem>();
             _playerManager = SystemManager.Instance.PlayerManager;
             _playerManager.PlayerCreateEvent.AddListener(PlayerCreateEvent);
@@ -88,21 +89,22 @@ namespace QT.Map
                 _iconObject = null;
             }
 
-            for (int i = 0; i < _uiLineRenderers.Length; i++)
+            for (int i = 0; i < _mapLineImages.Length; i++)
             {
-                _uiLineRenderers[i].enabled = false;
+                _mapLineImages[i].enabled = false;
             }
             _mapImage.enabled = false;
             _mapImage.color = _mapColors[2];
+            _mapImage.sprite = _mapSprites[2];
             _lineRenders.SetActive(false);
         }
 
         public void SetRouteDirection(MapDirection mapDirection)
         {
-            _uiLineRenderers[0].enabled = (mapDirection & MapDirection.Up) != 0;
-            _uiLineRenderers[1].enabled = (mapDirection & MapDirection.Down) != 0;
-            _uiLineRenderers[2].enabled = (mapDirection & MapDirection.Left) != 0;
-            _uiLineRenderers[3].enabled = (mapDirection & MapDirection.Right) != 0;
+            _mapLineImages[0].enabled = (mapDirection & MapDirection.Up) != 0;
+            _mapLineImages[1].enabled = (mapDirection & MapDirection.Down) != 0;
+            _mapLineImages[2].enabled = (mapDirection & MapDirection.Left) != 0;
+            _mapLineImages[3].enabled = (mapDirection & MapDirection.Right) != 0;
             _pathOpenDirection = mapDirection;
         }
         
@@ -124,6 +126,7 @@ namespace QT.Map
                 _mapImage.enabled = true;
                 _iconsTransform.gameObject.SetActive(true);
                 _mapImage.color = _mapColors[0];
+                _mapImage.sprite = _mapSprites[3];
                 if (_mapCellData == null)
                 {
                     _startColliderShapeSetting.AddListener(() =>
@@ -137,17 +140,28 @@ namespace QT.Map
                 }
                 //ColorSetLineRender(_mapColors[0]);
             }
-            else if (_dungeonMapSystem.GetCellData(CellPos).IsClear || _dungeonMapSystem.GetCellData(CellPos).IsVisited)
+            else if (_dungeonMapSystem.GetCellData(CellPos).IsClear)
+            {
+                _lineRenders.SetActive(true);
+                _mapImage.enabled = true;
+                _mapImage.color = _mapColors[0];
+                _mapImage.sprite = _mapSprites[0];
+                _iconsTransform.gameObject.SetActive(true);
+                //ColorSetLineRender(_mapColors[1]);
+            }
+            else if (_dungeonMapSystem.GetCellData(CellPos).IsVisited)
             {
                 _lineRenders.SetActive(true);
                 _mapImage.enabled = true;
                 _mapImage.color = _mapColors[1];
+                _mapImage.sprite = _mapSprites[1];
                 _iconsTransform.gameObject.SetActive(true);
-                //ColorSetLineRender(_mapColors[1]);
             }
             else if (_dungeonMapSystem.MultiPathClearCheck(CellPos))
             {
                 _mapImage.enabled = true;
+                _mapImage.color = _mapColors[1];
+                _mapImage.sprite = _mapSprites[2];
                 _iconsTransform.gameObject.SetActive(true);
             }
         }
