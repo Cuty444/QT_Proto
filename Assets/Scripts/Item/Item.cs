64b328c084f434da622471480c56a80bf377e7a5
Super-Ba <superbang175@gmail.com>
2023-05-24 16:25:56 +0900

    applypoint 개발

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using QT.Core;
 using QT.InGame;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace QT
 {
@@ -11,6 +12,8 @@ namespace QT
         private readonly int _itemDataId;
         public ItemGameData ItemGameData { get; private set; }
         public List<ItemEffect> ItemEffectData { get; private set; } = new ();
+        
+        private readonly Dictionary<ItemEffectGameData.ApplyPoints, UnityEvent> _applyPointEvents = new ();
 
         public Item(int itemDataId)
         {
@@ -34,9 +37,46 @@ namespace QT
             foreach (var effect in ItemEffectData)
             {
                 effect.ApplyEffect(player, this);
+
+                SetApplyPoint(effect, player);
             }
         }
 
+        private void SetApplyPoint(ItemEffect effect, Player player)
+        {
+            if (effect.ApplyPoints == ItemEffectGameData.ApplyPoints.Equip)
+            {
+                return;
+            }
+            
+            if (!_applyPointEvents.TryGetValue(effect.ApplyPoints, out var events))
+            {
+                events = new UnityEvent();
+                _applyPointEvents.Add(effect.ApplyPoints, events);
+            }
+
+            events.AddListener(() => ReapplyItemEffect(player, effect));
+        }
+        
+        private void ReapplyItemEffect(Player player, ItemEffect effect)
+        {
+            effect.RemoveEffect(player, this);
+            effect.ApplyEffect(player, this);
+        }
+        
+        public void InvokeApplyPoint(ItemEffectGameData.ApplyPoints applyPoints)
+        {
+            if (applyPoints == ItemEffectGameData.ApplyPoints.Equip)
+            {
+                return;
+            }
+            
+            if (_applyPointEvents.TryGetValue(applyPoints, out var events))
+            {
+                events.Invoke();
+            }
+        }
+        
         public void RemoveItemEffect(Player player)
         {
             foreach (var effect in ItemEffectData)
