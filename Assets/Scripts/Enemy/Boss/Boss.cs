e430e0eaf62e80405e161834c443072c4c801fb9
Super-Ba <superbang175@gmail.com>
2023-06-06 04:48:32 +0900

    보스 스크립트 추가

---
+++ Assets/Scripts/Enemy/Boss/Boss.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using QT.Core;
+using UnityEngine;
+
+
+namespace QT.InGame
+{
+    public class Boss : FSMPlayer<Enemy>, IFSMEntity
+    {
+        public enum States : int
+        {
+            Global,
+            
+            // 살아있음
+            Normal,
+            Rigid, 
+            
+            Dash,
+            Jump,
+            Attack,
+            Throw,
+            
+            Dead,
+        }
+        
+        public float ColliderRad { get; private set; }
+        [SerializeField] private int _enemyId;
+        public EnemyGameData Data { get; private set; }
+        public Rigidbody2D Rigidbody { get; private set; }
+        
+        
+        public EnemyProjectileShooter Shooter { get; private set; }
+        public Animator Animator { get; private set; }
+        public EnemySkeletalMaterialChanger MaterialChanger { get; private set; }
+        public Steering Steering { get; private set; }
+        
+        
+        private void Start()
+        {
+            Data = SystemManager.Instance.DataManager.GetDataBase<EnemyGameDataBase>().GetData(_enemyId);
+            Rigidbody = GetComponent<Rigidbody2D>();
+            Shooter = GetComponent<EnemyProjectileShooter>();
+            Animator = GetComponentInChildren<Animator>();
+            MaterialChanger = GetComponentInChildren<EnemySkeletalMaterialChanger>();
+            Steering = GetComponent<Steering>();
+
+            ColliderRad = SystemManager.Instance.DataManager.GetDataBase<ProjectileGameDataBase>()
+                .GetData(Data.ProjectileDataId).ColliderRad * 0.5f;
+            
+            //Shooter.Initialize(this);
+            
+            SetUp(States.Normal);
+        }
+        
+        public void SetPhysics(bool enable)
+        {
+            Rigidbody.simulated = enable;
+
+            var colliders = new Collider2D[Rigidbody.attachedColliderCount];
+            Rigidbody.GetAttachedColliders(colliders);
+            
+            foreach (var collider in colliders)
+            {
+                collider.enabled = enable;
+            }
+        }
+    }
+}
