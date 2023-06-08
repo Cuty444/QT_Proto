0788a4a17d6e71b579c5e27dbdea2339b596cef8
KimBeomsu <gohan2255@gmail.com>
2023-06-09 05:58:00 +0900

    방이동 치트키 추가

--- Assets/Scripts/TestCommands.cs
+++ Assets/Scripts/TestCommands.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using IngameDebugConsole;
 using QT.Core;
+using QT.Core.Map;
 using QT.InGame;
 using UnityEngine;
 
@@ -27,6 +28,25 @@ namespace QT
         {
             SystemManager.Instance.PlayerManager.Player.GetStatus(PlayerStats.MercyInvincibleTime).AddModifier(new StatModifier(999999,StatModifier.ModifierType.Addition,null));
             Debug.Log("Power Overwhelming");
+        }
+
+        [ConsoleMethod("Boss", "보스방 강제 이동")]
+        public static void Boss()
+        {
+            SystemManager.Instance.PlayerManager.PlayerMapPosition.Invoke(SystemManager.Instance.GetSystem<DungeonMapSystem>().DungeonMapData.BossRoomPosition - Vector2Int.down);
+            SystemManager.Instance.PlayerManager.PlayerDoorEnter.Invoke(Vector2Int.up);
+        }
+        
+        [ConsoleMethod("RoomMove", "방 생성순서 강제 이동")]
+        public static void RoomMove(int index)
+        {
+            if (index >= SystemManager.Instance.GetSystem<DungeonMapSystem>().DungeonMapData.MapNodeList.Count)
+            {
+                Debug.Log("방 최대치를 넘김 Index 숫자를 줄여주세요");
+                return;
+            }
+            SystemManager.Instance.PlayerManager.PlayerMapPosition.Invoke(SystemManager.Instance.GetSystem<DungeonMapSystem>().DungeonMapData.MapNodeList[index] - Vector2Int.down);
+            SystemManager.Instance.PlayerManager.PlayerDoorEnter.Invoke(Vector2Int.up);
         }
     }
 }
