33268e4f8d3cb061bbbcfb0a3a375beb6c756a5a
Super-Ba <superbang175@gmail.com>
2023-08-08 03:30:00 +0900

    텔레포트 개선
    날라가는 방향 거꾸로 된거 수정, 텔포 공격의 타겟을 찾을 때 거리도 고려하도록 개선

--- Assets/Scripts/Item/ItemEffect/TeleportItemEffect.cs
+++ Assets/Scripts/Item/ItemEffect/TeleportItemEffect.cs
@@ -55,8 +55,10 @@ namespace QT.InGame
             _player.OnAim.Invoke(aimDir);
             
             target.Item1.ResetProjectileDamage(damage);
-            target.Item1.ProjectileHit((target.Item1.Position - target.Item2.Position).normalized, shootSpeed,
+            target.Item1.ProjectileHit((target.Item2.Position - target.Item1.Position).normalized, shootSpeed,
                 BounceMask, ProjectileOwner.PlayerTeleport, 0, false);
+            
+            Debug.DrawLine(target.Item1.Position, target.Item2.Position, Color.green,3);
             
             SystemManager.Instance.ResourceManager.EmitParticle(SwingProjectileHitPath, target.Item1.Position);
             _player.Animator.SetTrigger(AnimationSwingHash);
@@ -92,8 +94,8 @@ namespace QT.InGame
             
             Enemy tpTarget = null;
             Enemy target = null;
-            float distance = float.MaxValue;
-            float hp = float.MaxValue;
+            float tpTargetPriority = float.MaxValue;
+            float targetPriority = float.MaxValue;
             
             foreach (var hitAble in hitAbles)
             {
@@ -107,17 +109,18 @@ namespace QT.InGame
 
                 if(enemy.HP <= 0 && enemy.CurrentStateIndex == (int)Enemy.States.Rigid && enemyDistance < _teleportDistance * _teleportDistance)
                 {
-                    if (enemyDistance < distance)
+                    if (enemyDistance < tpTargetPriority)
                     {
-                        distance = enemyDistance;
+                        tpTargetPriority = enemyDistance;
                         tpTarget = enemy;
                     }
                 }
                 else
                 {
-                    if (!enemy.IsDead &&enemy.HP < hp)
+                    var priority = (1000 * enemy.HP) + enemyDistance;
+                    if (!enemy.IsDead && priority < targetPriority)
                     {
-                        hp = enemy.HP;
+                        targetPriority = priority;
                         target = enemy;
                     }
                 }
