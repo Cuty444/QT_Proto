5f1cd603ba9c1556c831e054952c7df568c7d34f
Super-Ba <superbang175@gmail.com>
2023-08-18 11:59:08 +0900

    아이템 데이터베이스에 드랍 테이블을 통해 아이템 고르는 기능 추가

--- Assets/Scripts/Data/ItemGameData.cs
+++ Assets/Scripts/Data/ItemGameData.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using QT.Core;
+using QT.InGame;
+using QT.Util;
+using UnityEngine;
 
 namespace QT
 {
@@ -33,11 +37,24 @@ namespace QT
     [GameDataBase(typeof(ItemGameData), "ItemGameData")]
     public class ItemGameDataBase : IGameDataBase
     {
+        private const int MaxIteration = 1000;
+        
+        private readonly Dictionary<ItemGameData.GradeTypes, List<ItemGameData>> _itemGradeDictionary = new();
         private readonly Dictionary<int, ItemGameData> _datas = new();
 
         public void RegisterData(IGameData data)
         {
-            _datas.Add(data.Index, (ItemGameData)data);
+            var itemData = (ItemGameData)data;
+            
+            _datas.Add(data.Index, itemData);
+            
+            
+            if(!_itemGradeDictionary.TryGetValue(itemData.GradeType, out var list))
+            {
+                _itemGradeDictionary.Add(itemData.GradeType, list = new List<ItemGameData>());
+            }
+
+            list.Add(itemData);
         }
 
         public ItemGameData GetData(int id)
@@ -49,5 +66,46 @@ namespace QT
 
             return null;
         }
+        
+        public ItemGameData[] GetGradeDatas(ItemGameData.GradeTypes gradeType)
+        {
+            if (_itemGradeDictionary.TryGetValue(gradeType, out var value))
+            {
+                return value.ToArray();
+            }
+
+            return null;
+        }
+
+        public ItemGameData PickRandom(ItemGameData.GradeTypes gradeType)
+        {
+            if (_itemGradeDictionary.TryGetValue(gradeType, out var list))
+            {
+                if (list.Count > 0)
+                {
+                    return list[Random.Range(0, list.Count)];
+                }
+            }
+
+            return null;
+        }
+
+        public List<ItemGameData> GetItemsWithDropPercentage(DropPercentage percentage, int count, Inventory inventory)
+        {
+            var result = new List<ItemGameData>();
+
+            for (int i = 0; result.Count < count && i < MaxIteration; i++)
+            {
+                var item = PickRandom(percentage.RandomGradeType());
+                
+                if (item != null && !result.Contains(item) && !inventory.Contains(item.Index))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+        
     }  
 }
