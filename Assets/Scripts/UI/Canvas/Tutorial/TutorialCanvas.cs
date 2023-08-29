170b29278c7de6d8c70b8c391b5f62cf5c91dfc8
Super-Ba <superbang175@gmail.com>
2023-08-30 02:28:43 +0900

    튜토리얼 캔버스 개선

---
+++ Assets/Scripts/UI/Canvas/Tutorial/TutorialCanvas.cs
@@ -0,0 +1,70 @@
+using QT.Core;
+using QT.UI;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+using UnityEngine.Video;
+
+namespace QT.Tutorial
+{
+    public class TutorialCanvas : UIPanel
+    {
+        [SerializeField] private VideoClip[] _videoClips;
+        
+        [SerializeField] private RectTransform _videoParent;
+        [SerializeField] private VideoPlayer _videoPlayer;
+        
+        [SerializeField] private HorizontalScrollSnap _scrollSnap;
+        
+        [SerializeField] private UITweenAnimator _popAnimation;
+        
+        public override void OnOpen()
+        {
+            base.OnOpen();
+            OnPageChanged(0);
+            
+            _popAnimation.ReStart();
+        }
+
+        public override void OnClose()
+        {
+            base.OnClose();
+            if (SystemManager.Instance.UIManager.GetUIPanel<UIDiaryCanvas>()._isTutorial)
+                SystemManager.Instance.UIManager.GetUIPanel<UIDiaryCanvas>().TutorialClose();
+            else
+                SystemManager.Instance.UIManager.GetUIPanel<TitleCanvas>().TutorialClose();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) OnClose();
+            
+            if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                _scrollSnap.PreviousScreen();
+            else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                _scrollSnap.NextScreen();
+        }
+
+        public void OnPageChangeStart()
+        {
+            _videoParent.gameObject.SetActive(false);
+        }
+
+        public void OnPageChanged(int index)
+        {  
+            if (index < 0 || index >= _videoClips.Length || _videoClips[index] == null)
+            {
+                _videoParent.gameObject.SetActive(false);
+                return;
+            }
+            
+            _scrollSnap.CurrentPageObject(out var page);
+            
+            _videoParent.SetParent(page.transform);
+            _videoParent.anchoredPosition = Vector3.zero;
+            _videoParent.gameObject.SetActive(true);
+            
+            _videoPlayer.clip = _videoClips[index];
+            _videoPlayer.Play();
+        }
+    }
+}
